66957fe509e988a5ef784d9a91b2d029c15157ce
Asante Cureton <asantercureton@gmail.com>
2021-09-14 22:51:24 -0400

    Updated Employee.js file for Employee Class

---
+++ Employee.js
@@ -0,0 +1,33 @@
+class Employee {
+  constructor(name, title, id, email, gitHub) {
+    // TODO: Add code here
+    this.name = name;
+    this.title = title;
+    this.id = id;
+    this.email = email;
+    this.gitHub = gitHub;
+  };
+
+
+  getName(){
+    return this.name;
+  };
+
+  getId(){
+    return this.id;
+  };
+
+  getTitle(){
+    return this.title;
+  };
+
+  getEmail(){
+    return this.email;
+  };
+
+  getGitHub(){
+    return this.gitHub;
+  };
+}
+
+module.exports = Employee;
